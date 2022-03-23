dfc2db40dd0c1e05079aae196969f87d96b43250
JawaraGordon <jawaragordon@icloud.com>
2022-03-23 09:58:18 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+// const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser} + !`;
+
+// const shortGreeting = 'Welcome, '
+
+// const shortGreeting = `Welcome, ${currentUser}`
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
